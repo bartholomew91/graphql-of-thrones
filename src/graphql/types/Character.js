6f3f12ce9cff51f16f8b9a9b8c91711f6032fa8c
bartholomew91 <matt.johnston91@gmail.com>
2018-03-08 22:15:24 -0600

    Updated character to support allegiance, successor, father

--- src/graphql/types/Character.js
+++ src/graphql/types/Character.js
@@ -12,6 +12,7 @@ const db = require('../../config/dbconnection').get(); // grab the db instance
 const jsonUtils = require('../../utils/json');
 
 const TitleType = require('./Title');
+const HouseType = require('./House');
 
 const CharacterType = new GraphQL.GraphQLObjectType({
     name: 'Character',
@@ -35,7 +36,10 @@ const CharacterType = new GraphQL.GraphQLObjectType({
             description: 'Titles held by characters',
             resolve: character => { // refactor needed
                 if(character.titles != '') {
-                    const query = `SELECT * FROM titles WHERE id IN ('${jsonUtils.toCommaDelim(character.titles, val => val.id)}')`;
+                    const query = {
+                        text: 'SELECT * FROM titles WHERE id IN($1)',
+                        values: [jsonUtils.toCommaDelim(character.titles, val => val.id)]
+                    }
                     return db.query(query)
                         .then( (response) => {
                             return response.rows;
@@ -48,20 +52,78 @@ const CharacterType = new GraphQL.GraphQLObjectType({
             type: GraphQLBoolean,
             description: 'Denotes if a character is alive or not',
         },
-        spouse: {
-            type: new GraphQLList(CharacterType),
-            description: 'Spouse(s) for the character',
-            resolve: character => { // refactor needed
-                if(character.spouse != null && character.spouse.length > 0) {
-                    const query = `SELECT * FROM characters WHERE id IN ('${jsonUtils.toCommaDelim(character.spouse, val => val.id)}')`;
+        death: {
+            type: GraphQLString,
+            description: 'Provides information about the characters death'
+        },
+        allegiance: {
+            type: new GraphQLList(HouseType),
+            description: 'House(s) the character belongs to',
+            resolve: character => {
+                if(character.allegiance.length > 0) {
+                    const query = {
+                        text: 'SELECT * FROM houses WHERE id IN($1)',
+                        values: [jsonUtils.toCommaDelim(character.allegiance, val => val.id)]
+                    }
                     return db.query(query)
-                        .then( (response) => {
+                        .then( response => {
+                            return response.rows;
+                        })
+                }
+                return null;
+            }
+        },
+        successor: {
+            type: new GraphQLList(CharacterType),
+            description: 'The characters successor',
+            resolve: character => {
+                if(character.successor.length > 0) {
+                    const query = {
+                        text: 'SELECT * FROM characters WHERE id IN($1)',
+                        values: [jsonUtils.toCommaDelim(character.successor, val => val.id)]
+                    }
+                    return db.query(query)
+                        .then( response => {
                             return response.rows;
                         });
                 }
                 return null;
             }
-        }
+        },
+        spouse: {
+            type: new GraphQLList(CharacterType),
+            description: 'Spouse(s) for the character',
+            resolve: character => { // refactor needed
+                if(character.spouse != null && character.spouse.length > 0) {
+                    const query = {
+                        text: 'SELECT * FROM characters WHERE id IN($1)',
+                        values: [jsonUtils.toCommaDelim(character.spouse, val => val.id)]
+                    }
+                    return db.query(query)
+                        .then( response => {
+                            return response.rows;
+                        });
+                }
+                return null;
+            }
+        },
+        father: {
+            type: new GraphQLList(CharacterType),
+            description: 'Father of the character',
+            resolve: character => { // refactor needed
+                if(character.father.length > 0) {
+                    const query = {
+                        text: 'SELECT * FROM characters WHERE id IN($1)',
+                        values: [jsonUtils.toCommaDelim(character.father, val => val.id)]
+                    }
+                    return db.query(query)
+                        .then( response => {
+                            return response.rows;
+                        });
+                }
+                return null;
+            }
+        },
     })
 });
 
